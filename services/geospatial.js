08b31e2c60e220bf56029e8e3e5a1be7df4c0a57
Deividas Jackus <d@jackus.eu>
2019-06-07 03:23:25 +0200

    add logic for calculating direction

--- services/geospatial.js
+++ services/geospatial.js
@@ -7,6 +7,29 @@ const nearestPoint = require("@turf/nearest-point").default;
 
 const { intersections } = require("../db/geospatial");
 
+// Based on https://stackoverflow.com/a/25867068/9282204
+function degreesToCardinalDirection(degrees) {
+  const directions = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW",
+  ];
+  return directions[Math.floor(degrees / 22.5 + 0.5) % 16];
+}
+
 function getNearestIntersection({ latitude, longitude }) {
   const givenPoint = point([longitude, latitude]);
 
@@ -18,6 +41,9 @@ function getNearestIntersection({ latitude, longitude }) {
       ),
     ),
   );
+
+  const bearing = getBearing(nearestIntersection, givenPoint); // angle of the point in relation to the intersection
+  const direction = degreesToCardinalDirection(bearing); // cardinal direction of the point in relation to the intersection
 
   return {
     point: {
@@ -31,7 +57,8 @@ function getNearestIntersection({ latitude, longitude }) {
       longitude: getCoord(nearestIntersection)[0],
     },
     distance: nearestIntersection.properties.distanceToPoint,
-    bearing: getBearing(nearestIntersection, givenPoint),
+    bearing,
+    direction,
   };
 }
 
