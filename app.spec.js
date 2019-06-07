eb529a81e73a58bb8ddfae19c63974e6cd030b6f
Deividas Jackus <d@jackus.eu>
2019-06-07 09:00:44 +0200

    update application server tests

--- app.spec.js
+++ app.spec.js
@@ -6,18 +6,25 @@ const chaiHttp = require("chai-http");
 const app = require("./app");
 const packageInfo = require("./package.json");
 
-chai.use(chaiHttp);
 const { expect } = chai;
+chai.use(chaiHttp);
 
 describe("API server", () => {
-  it("should return JSON", async () => {
+  it("should return valid HTTP response codes for successful requests", async () => {
+    expect(await chai.request(app).get("/")).to.have.status(200);
+  });
+
+  it("should return valid HTTP response codes for unsuccessful requests", async () => {
+    expect(await chai.request(app).get("/non-existent")).to.have.status(404);
+  });
+
+  it("should return valid JSON", async () => {
     expect(await chai.request(app).get("/"))
-      .to.have.status(200)
       .and.to.be.json.and.to.have.own.property("body")
       .that.is.a("object");
   });
 
-  it("should expose project version", async () => {
+  it("should expose version", async () => {
     expect((await chai.request(app).get("/")).body).to.have.property("version", packageInfo.version);
   });
 });
